bcaad5727b6ae2b13148ba91442ebdbda0569491
Mitigence <mitigence@gmail.com>
2025-04-22 17:51:02 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogUploader from './components/LogUploader';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LogWeaver: AI-Powered Log Parser</h1>
+      <LogUploader />
+    </div>
+  );
+}
+
+export default App;
